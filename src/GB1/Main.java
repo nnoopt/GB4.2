68ebac1d92adba752c8589fbb7e40e4932d1e1fd
nnoopt <nnoopt@gmail.com>
2020-12-08 20:22:19 +0300

    homeWork_2

--- src/GB1/Main.java
+++ src/GB1/Main.java
@@ -123,7 +123,7 @@ public class Main {
 
     public static void line(){
         System.out.println();
-        System.out.println("--------------------------------------------------------------------------");
+        System.out.println("-------------------------------------------------------------------------");
     }
 }
 
