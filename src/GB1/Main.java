d11aa16292ffb10c810fb44c5bc62e482aabc348
nnoopt <nnoopt@gmail.com>
2020-12-14 04:28:39 +0300

    homeWork_4

--- src/GB1/Main.java
+++ src/GB1/Main.java
@@ -5,99 +5,192 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
 
-            String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
-            String s = " Предложение один     Теперь предложение  два    Тут предложение   три";
+    static int X = 5;
+    static int Y = 5;
+    static int L = 4;
 
-            one(3, 10);
-            two(words);
-            three (s);
+    static char[][] field = new char[Y][X];
+
+    static char human = '+';
+    static char computer = 'o';
+    static char emptiness = '.';
+
+    public static void goMass() {
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                field[i][j] = emptiness;
+            }
+        }
     }
 
 
-    public static void one(int attempts, int variants) {
+    public static void printField() {
+        for (int i = 0; i < field.length; i++) {
+            System.out.println();
+            for (int j = 0; j < field[i].length; j++) {
+                System.out.print("| " + field[i][j] + " ");
+                if (j == field[i].length - 1) System.out.println("|");
+            }
+        }
+        System.out.println("=======================================");
+    }
 
-        System.out.println("Задание 1");
+    public static void go(char c) {
+        System.out.println("Введи координаты");
+
+        int x;
+        int y;
+
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Веди X:");
+            x = scanner.nextInt() - 1;
+            System.out.println("Веди Y:");
+            y = scanner.nextInt() - 1;
+
+            if (isCorrectGo(y, x)) {
+                field[y][x] = c;
+                break;
+            } else System.out.println("Ошибка. Введи корректные координаты.");
+
+        }
+    }
+
+    public static void goAI() {
+        Random random = new Random();
+
+        int manager = 0;
+
+            for (int i = 0; i <= field.length - L; i++) {
+
+                if (manager>=1)break;
+                for (int j = 0; j <= field[i].length - L; j++) {
+
+                    if (manager>=1)break;
+                    for (int k = 0; k < L; k++) {
+
+                        if (manager>0)break;
+
+                        for (int l = 0; l < L; l++) {
+                            if (field[k + i][l + j] == computer) {
+                                    if ((k + i+1) <= field.length-1 && field[k + i + 1][l + j] == emptiness) {
+                                        field[k + i + 1][l + j] = computer;
+                                        manager++;
+                                        break;
+                                   }
+                                    if ((k + i-1) >= 0 && field[k + i - 1][l + j] == emptiness) {
+                                        field[k + i - 1][l + j] = computer;
+                                        manager++;
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+            }
+
+            while (true) {
+                if (manager>=1)break;
+                int y = random.nextInt(Y);
+                int x = random.nextInt(X);
+
+                if (isCorrectGo(y, x)) {
+                    field[y][x] = computer;
+                    break;
+                }
+            }
+
+    }
+
+
+    public static boolean isCorrectGo(int y, int x) {
+        if (x < X && x >= 0 && y < Y && y >= 0 && field[y][x] == emptiness) return true;
+        return false;
+    }
+
+    public static boolean emptyDots(char c[][]) {
+        for (int i = 0; i < c.length; i++) {
+            for (int j = 0; j < c[i].length; j++) {
+                if (c[i][j] == emptiness) return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean win(char c, char[][] mass) {
+
+        int countX = 0;
+        int countY = 0;
+        int countD1 = 0;
+        int countD2 = 0;
+
+        for (int i = 0; i <= mass.length-L; i++) {
+            for (int j = 0; j <= mass[i].length-L; j++) {
+                for (int k = 0; k < L; k++) {
+                    countX = 0;
+                    countY = 0;
+                    for (int l = 0; l < L; l++) {
+                        if (field[k+i][l+j]==c)countX++;
+                        else countX=0;
+
+                        if (field[l+j][k+i]==c)countY++;
+                        else countY=0;
+
+                        if ((k==l) && field [k+i][l+j]==c)countD1++;
+                        if ((k==l) && field [k+i][l+j]!=c)countD1=0;
+
+                        if (k == ((L-1-l)) && field [k+i][l+j]==c) countD2++;
+                        if (k == ((L-1-l)) && field [k+i][l+j]!=c) countD2=0;
+
+                        if (countD1==L||countD2==L||countX==L||countY==L)return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+
+
+    public static void main(String[] args) {
+        goMass();
 
         while (true) {
 
-        System.out.println("Угадай число от 0 до " + (variants-1) + "!");
-            Random random = new Random();
-            Scanner sc = new Scanner(System.in);
-            int r = random.nextInt(10);
+            printField();
+            go(human);
 
-            for (int i = attempts; i > 0; i--) {
-
-                System.out.println("У тебя осталось попыток: " + i);
-                System.out.println("Введи число ...");
-
-                int s = sc.nextInt();
-
-                if (s == r) {
-                    System.out.println("Ура, ты угодал!");
-                    break;
-                } else {
-                    System.out.println("Ты не угодал");
-                    if (s > r) System.out.println("Твое число сшишком большое ..");
-                    else System.out.println("Твое число сшишком маленькое .. ");
-                }
-            }
-            int res;
-
-            while (true) {
-                System.out.println("Повторить игру? 1 - если НЕТ, 0 - если ДА");
-                res = sc.nextInt();
-                if (res == 1) break;
-                if (res == 0) break;
-            }
-
-            if (res == 1) {
-                System.out.println("Конец игры!");
-                System.out.println("===================================");
+            if (win(human, field)) {
+                System.out.println("Ура! Человек победил!");
+                printField();
                 break;
             }
 
-        }
-    }
 
-
-    public static void two (String [] words){
-        Random random = new Random();
-        String wordComputer = words[random.nextInt(words.length)];
-        System.out.println("Угадай слово!");
-        Scanner scn = new Scanner(System.in);
-        StringBuilder stringBuilder = new StringBuilder("###############");
-        while (true) {
-
-            String wordPeople = scn.nextLine();
-
-            for (int i = 0; i < wordComputer.length() && i<wordPeople.length(); i++) {
-                if (wordComputer.charAt(i) == wordPeople.charAt(i)) stringBuilder.setCharAt(i, wordComputer.charAt(i));
+            if (!emptyDots(field)) {
+                System.out.println("Ничья!");
+                printField();
+                break;
             }
-            System.out.println(stringBuilder);
-            if (wordPeople.equalsIgnoreCase(wordComputer)) break;
-        }
-        System.out.println("Ура, ты угадал слово!");
-        System.out.println("===================================");
-    }
 
-    public static void three (String s){
-        System.out.println(s);
-        String s1 = s.replaceAll(" +", " ");
+            goAI();
+            if (win(computer, field)) {
+                System.out.println("Ура! ИИ победил!");
+                printField();
+                break;
+            }
 
-        StringBuilder s2 = new StringBuilder(s1);
-
-        for (int  i = s1.length()-1; i>= 0 ; i--) {
-            if (s1.charAt(i)>= 'А' && s1.charAt(i)<= 'Я'){
-                s2.setCharAt(i-1, '.');
-                s2.insert(i, ' ');
+            if (!emptyDots(field)) {
+                System.out.println("Ничья!");
+                printField();
+                break;
             }
         }
-
-        s2.delete(0, 2);
-        if (s2.charAt(s2.length()-1)!='.') s2.append('.');
-        System.out.println(s2);
-        System.out.println("===================================");
     }
 }
+
+
+// не длоделал(
+
